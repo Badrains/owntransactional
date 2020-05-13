85522a23817b50adad537f2ba8ff44870164b6e9
wangdong <305306733@qq.com>
2020-05-13 17:57:23 +0800

    第一次提交

---
+++ src/main/java/com/badrains/owntransactional/OwntransactionalApplication.java
@@ -0,0 +1,13 @@
+package com.badrains.owntransactional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwntransactionalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OwntransactionalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/badrains/owntransactional/OwntransactionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.badrains.owntransactional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwntransactionalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
